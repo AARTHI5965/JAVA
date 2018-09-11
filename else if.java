ec324470b428ca6162db7bf949db0f34682fc094
AARTHI5965 <43039425+AARTHI5965@users.noreply.github.com>
2018-09-11 15:12:28 +0530

    Add files via upload

---
+++ else if.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    	int rupees;
+      rupees=5000;
+      if(rupees>=20000)
+        System.out.println("Free Trolley Suitcase");
+      else if (rupees>=10000)
+        System.out.println("Free Travel Bag");
+      else if(rupees>=5000)
+        System.out.println("Free BackPck");
+      else 
+        System.out.println("Collect money even for cover we give");
+  }
+}
