f512599c9aa1d2230fac35c71536cca6e2e27afa
AARTHI5965 <43039425+AARTHI5965@users.noreply.github.com>
2018-10-04 12:29:06 +0530

    Add files via upload

--- odd or even.java
+++ odd or even.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+   
    int number;
    Scanner read = new Scanner(System.in);
-   System.out.print("Enter the number you want to check:");
+   System.out.print("");
    number = read.nextInt();
    read.close();
   if(number%2==0)
-        
     System.out.println("even");
   else if(number%2!=0)
-     System.out.println("odd");
-  else
+    System.out.println("odd");
+  else 
      System.out.println("invalid");
   }
 }
