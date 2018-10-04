6dcd8190f5cd3ce40b4a28bc4d5daf70615dd75f
AARTHI5965 <43039425+AARTHI5965@users.noreply.github.com>
2018-10-04 19:24:27 +0530

    Add files via upload

---
+++ Greater number .java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+ class Dcoder
+ { 
+	public static void main(String args[])
+ 	{
+ 		int a,b,c;
+ 	Scanner read = new        
+ 	Scanner(System.in);
+
+   System.out.print("");
+
+   a= read.nextInt();
+   b= read.nextInt();
+   c= read.nextInt();
+
+   read.close();
+  
+      if(a>b && a>c )
+      System.out.println(a);
+      if(b>a && b>c)
+      System.out.println(b);
+      if(c>a && c>b)
+      System.out.println(c);
+      // your code goes here
+	}
+}
+   
+        
+      
+	
+	
+ 	
+
+ 	
+ 
+    
