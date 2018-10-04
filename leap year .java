a95a967d7fa846471caeb1df1b9e3b386c6caab6
AARTHI5965 <43039425+AARTHI5965@users.noreply.github.com>
2018-10-04 17:45:16 +0530

    Add files via upload

---
+++ leap year .java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ class Dcoder
+ { 
+	public static void main(String args[])
+ 	{
+ 	
+ int number;
+
+  Scanner read = new        
+  Scanner(System.in);
+
+   System.out.print("");
+
+   number = read.nextInt();
+
+   read.close();
+   if(number %4==0)
+        System.out.println("leap year");
+        
+      
+	}
+}	
+ 	
+
+ 	
+ 
