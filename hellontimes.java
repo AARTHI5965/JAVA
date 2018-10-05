bce2c153fb9bc6c305ec5f4fbf684193868fdf3d
AARTHI5965 <43039425+AARTHI5965@users.noreply.github.com>
2018-10-05 17:08:12 +0530

    Add files via upload

---
+++ hellontimes.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+ class Dcoder
+ { 
+	public static void main(String args[])
+ 	{ 
+
+		int number;
+
+  Scanner num = new        
+  Scanner(System.in);
+  number = num.nextInt();
+
+   num.close();
+   for(int i=0;i<number;i++)
+        System.out.println("Hello");
+
+ 	}
+ }
